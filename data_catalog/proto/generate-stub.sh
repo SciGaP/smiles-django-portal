bbe94c0f7856313b026be3e748e75a0da17390f2
Lahiru Jayathilake <lahirujayathilake@gmail.com>
2023-03-01 23:59:45 +0530

    protobuf files and stubs for SMILES data products

--- data_catalog/proto/generate-stub.sh
+++ data_catalog/proto/generate-stub.sh
@@ -1,1 +1,3 @@
 python -m grpc_tools.protoc -I . --python_out=. ./computational_dp.proto
+python -m grpc_tools.protoc -I . --python_out=. ./experimental_dp.proto
+python -m grpc_tools.protoc -I . --python_out=. ./literature_dp.proto
