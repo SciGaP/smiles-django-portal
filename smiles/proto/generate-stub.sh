f6125673fb9777efba0c426dabe18c64e9a94fca
Miao <auroram0703@gmail.com>
2025-03-24 11:36:23 -0400

    Revert changes in generate-stub.sh

--- smiles/proto/generate-stub.sh
+++ smiles/proto/generate-stub.sh
@@ -4,5 +4,3 @@ python -m grpc_tools.protoc -I . --python_out=. ./literature_dp.proto
 
 # To generate the DataCatalog Stubs use the following command
 #python -m grpc_tools.protoc -I . --python_out=. --grpc_python_out=. ./data_catalog.proto
-# Generate the DataCatalog Stubs
-python -m grpc_tools.protoc -I "D:/airavata-data-catalog/data-catalog-api/stubs/src/main/proto" --python_out="D:/smiles-django-portal/smiles/proto" --grpc_python_out="D:/smiles-django-portal/smiles/proto" "D:/airavata-data-catalog/data-catalog-api/stubs/src/main/proto/data_catalog.proto"
